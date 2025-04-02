6fafcb98ef0fda98f2b51b16e79cade2724ab59e
STANISLAV PRIKHODKO <BusyStas@users.noreply.github.com>
2025-04-02 14:26:04 +0000

    depin links

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/depin')
+def depin():
+    return render_template('depin.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
